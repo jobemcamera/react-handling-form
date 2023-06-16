3398a8c78c958a9ad7905af55b28ebf43ccda5f2
jobe-lumni <jobecamera@gmail.com>
2023-06-16 09:14:58 -0300

    setando o estado do input Name e acessando o valor digitado. Mostrado tambem como usar o useRef

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,35 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+
+
+    const enteredValue = nameInputRef.current.value; 
+    console.log(enteredValue);
+    setEnteredName('');
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          type='text' 
+          id='name' 
+          onChange={nameInputChangeHandler} 
+          value={enteredName} 
+          ref={nameInputRef} 
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
