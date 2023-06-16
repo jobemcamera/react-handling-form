2e3b0a567e55ca1e10ae88118059258b3c1ec5ed
jobe-lumni <jobecamera@gmail.com>
2023-06-16 09:29:48 -0300

    refatorando o tratamento de erro do input vazio com toque do usuario

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -4,7 +4,8 @@ const SimpleInput = (props) => {
 
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -12,6 +13,8 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+
+    setEnteredNameTouched(true);
 
     if (enteredName.trim() === '') {
       setEnteredNameIsValid(false);
@@ -25,7 +28,9 @@ const SimpleInput = (props) => {
     setEnteredName('');
   }
 
-  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
 
   return (
     <form onSubmit={formSubmissionHandler}>
@@ -38,7 +43,7 @@ const SimpleInput = (props) => {
           value={enteredName} 
           ref={nameInputRef} 
         />
-        {!enteredNameIsValid && <p className='error-text'>Name must not be empty.</p>}
+        {nameInputIsInvalid && <p className='error-text'>Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
