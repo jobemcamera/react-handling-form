d00cbe6013dfdf3d33c78d9a5fd058a1099b8296
jobe-lumni <jobecamera@gmail.com>
2023-06-16 09:48:45 -0300

    refatoracao da logica de erro para deixar mais clean e reutilizavel

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,27 +1,19 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 const SimpleInput = (props) => {
 
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
-
-    if (event.target.value.trim() != '') {
-      setEnteredNameIsValid(true);
-    }
-  };
+  }
 
   const nameInputBlurHandler = (event) => {
-    setEnteredNameTouched(true);
-
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
-    }
-    
+    setEnteredNameTouched(true);  
   }
 
   const formSubmissionHandler = (event) => {
@@ -29,20 +21,14 @@ const SimpleInput = (props) => {
 
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
 
-    setEnteredNameIsValid(true);
-
-    const enteredValue = nameInputRef.current.value; 
-    console.log(enteredValue);
     setEnteredName('');
+    setEnteredNameTouched(false);
   }
-
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
-
+  
   const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
 
   return (
@@ -55,7 +41,6 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler} 
           onBlur={nameInputBlurHandler}
           value={enteredName} 
-          ref={nameInputRef} 
         />
         {nameInputIsInvalid && <p className='error-text'>Name must not be empty.</p>}
       </div>
